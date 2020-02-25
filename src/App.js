300e44c29b46fdbb617966d98e74daa0057603b9
Paul Hung <paulhung@Pauls-MacBook-Pro.local>
2020-02-24 22:21:52 -0800

    Navbar with fullpage scrolling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import ReactFullpage from '@fullpage/react-fullpage';
 import About from './components/about.js'
 import Experience from './components/experience.js'
 
@@ -26,12 +28,85 @@ import Experience from './components/experience.js'
 //     </div>
 //   );
 // }
+
+const Fullpage = (
+  <ReactFullpage
+    //fullpage options
+    licenseKey = {'YOUR_KEY_HERE'}
+    scrollingSpeed = {1000} /* Options here */
+
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <button onClick={() => fullpageApi.moveTo(2)}>
+                  About
+              </button>
+              <button onClick={() => fullpageApi.moveTo(3)}>
+                  Experience
+            </button> 
+          </div>
+          <div className="section">
+            <About></About>            
+          </div>
+          <div className="section">
+            <Experience></Experience>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
+const anchors = ["firstPage", "secondPage"];
+
 function App() {
   return (
     <div>
-      <About></About>
-      <Experience></Experience>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark" id = "navbar">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <div className = "collapse navbar-collapse">
+          <ul className = "navbar-nav mr-auto" id = "myMenu">
+            <li data-menuanchor="firstPage" className="nav-item active"><a className="nav-link" href="#firstPage">About</a></li>
+            <li data-menuanchor="secondPage" className="nav-item"><a className="nav-link" href="#secondPage">Experience</a></li>
+          </ul>
+        </div>
+      </nav>
+      {/* <ul id="myMenu">
+        <li data-menuanchor="firstPage" class="active"><a href="#firstPage">First section</a></li>
+        <li data-menuanchor="secondPage"><a href="#secondPage">Second section</a></li>
+        <li data-menuanchor="thirdPage"><a href="#thirdPage">Third section</a></li>
+      </ul> */}
+      <ReactFullpage
+      //fullpage options
+      licenseKey = {'YOUR_KEY_HERE'}
+      anchors={anchors}
+      navigation
+      menu = '#myMenu'
+      scrollingSpeed = {1000} /* Options here */
+
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            {/* <div className="section">
+              <button onClick={() => fullpageApi.moveTo(2)}>
+                    About
+                </button>
+                <button onClick={() => fullpageApi.moveTo(3)}>
+                    Experience
+              </button> 
+            </div> */}
+            <div className="section">
+              <About></About>            
+            </div>
+            <div className="section">
+              <Experience></Experience>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+      />
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactFullpage from '@fullpage/react-fullpage';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// const Fullpage = () => (
+//     <ReactFullpage
+//       //fullpage options
+//       licenseKey = {'YOUR_KEY_HERE'}
+//       scrollingSpeed = {1000} /* Options here */
+  
+//       render={({ state, fullpageApi }) => {
+//         return (
+//           <ReactFullpage.Wrapper>
+//             <div className="section">
+//               <p>Section 1 (welcome to fullpage.js)</p>
+//               <button onClick={() => fullpageApi.moveSectionDown()}>
+//                 Click me to move down
+//               </button>
+//             </div>
+//             <div className="section">
+//               <p>Section 2</p>
+//             </div>
+//           </ReactFullpage.Wrapper>
+//         );
+//       }}
+//     />
+//   );
+  
+// ReactDOM.render(<Fullpage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
